3c71495a1228d64cb062665bc7c94c694cd45c52
adrielsitumorang <adrielsitumorang@gmail.com>
2021-04-10 01:41:51 +0700

    Membuat halaman pertama

---
+++ front-end/src/Rute/rute.js
@@ -0,0 +1,9 @@
+const Routes = {}
+
+Routes.host = "http://localhost:3000"
+Routes.home = "/"
+Routes.pemesanan1 = Routes.home + "pemesanan/pertama/"
+Routes.pemesanan2 = Routes.home + "pemesanan/kedua/"
+Routes.pembayaran = Routes.home + "pembayaran"
+
+export default Routes;

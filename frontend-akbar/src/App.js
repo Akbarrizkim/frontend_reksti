c776a61cd976a93767db0da6b2114a875f657883
Akbarrizkim <akbarezeki@gmail.com>
2021-04-10 18:36:07 +0700

    first commit in branch akbar

---
+++ frontend-akbar/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Home } from './contents/Home';
+import { About } from './contents/About';
+import { Contact } from './contents/Contact';
+import { NoMatch } from './contents/NoMatch';
+import { Layout } from './components/Layout';
+import { NavigationBar } from './components/NavigationBar';
+
+function App() {
+  return (
+    <React.Fragment>
+      <NavigationBar />
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </Layout>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ frontend-akbar/src/contents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => (
+    <div>
+        <h2>About Page</h2>
+        <p>My name is Akbar</p>
+        <p>I'm 21 years old and I'm from Bogor.</p>
+        <p>I am an undergraduate student from Bandung Institute of Technology</p>
+    </div>
+)

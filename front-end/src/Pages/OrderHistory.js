c66ab31f548b615ab93f11292443e39151152037
Akbarrizkim <akbarezeki@gmail.com>
2021-04-19 19:18:58 +0700

    integrated front-end and back-end for order history

--- front-end/src/Pages/OrderHistory.js
+++ front-end/src/Pages/OrderHistory.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HistoryCard from '../Components/HistoryCard';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const Styles = styled.div`
     .wrapper {
@@ -10,17 +11,40 @@ const Styles = styled.div`
     }
 `;
 
-export const OrderHistory = () => (
-    <Styles>
-        <div className="container">
-            <div className="wrapper">
-                <h1>Order History Pelanggan</h1>
+export const OrderHistory = () => {
+    const [state, setState] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios('http://139.59.235.181:8800/order',);
+
+            setState(result.data);
+            console.log(result.data);
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <Styles>
+            <div className="container">
+                <div className="wrapper">
+                    <h1>Riwayat Pesanan Pelanggan</h1>
+                </div>
+                
+                {state.data?.map(item => (
+                    <HistoryCard key={item.order_id} idOrder={item.order_id} dateOrder={item.order_date} priceOrder={item.total_price} />
+                ))}
             </div>
-            
-            <HistoryCard />
-            <HistoryCard />
-            <HistoryCard />
-            <HistoryCard />
-        </div>
-    </Styles>
-)
+
+            {/* <div>
+                <ul>
+                    {state.data?.map(item => (
+                        <li key={item.order_id}>
+                            <p>{item.order_date}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div> */}
+        </Styles>
+    );
+}

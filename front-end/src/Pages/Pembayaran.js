bcc89013007c37cdae85172fc25989719c7e5b8d
adrielsitumorang <adrielsitumorang@gmail.com>
2021-04-20 04:20:30 +0700

    Melakukan integrasi pada bagian riwayat pembayaran

--- front-end/src/Pages/Pembayaran.js
+++ front-end/src/Pages/Pembayaran.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Table,CardGroup,Modal,Card} from 'react-bootstrap';
 import styled from 'styled-components';
 import ovoImage from '../Assets/Ovo.png';
 import gopayImage from '../Assets/gopay.png';
 import linkajaImage from '../Assets/linkaja.png';
+import axios from 'axios';
 
 const StyledTable = styled(Table)`
     margin-top: 30px;
@@ -58,99 +59,107 @@ const Isi = styled.p`
     margin-left: 20px;
 `;
 
-export const Pembayaran = () => (
-    <div class="parent-container d-flex">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <Styledjudul>
-                        Riwayat Pembayaran Berhasil
-                    </Styledjudul>
-                    <StyledTable striped bordered hover>
-                     <thead>
-                         <tr>
-                             <th >Nomor Order</th>
-                             <th >Tanggal Pemesanan</th>
-                             <th >Lokasi Pengantaran</th>
-                             <th >Total Harga</th>
-                         </tr>
-                     </thead>
-                     <tbody>
-                         <tr>
-                             <td>1</td>
-                             <td>10 April 2021</td>
-                             <td>ITB</td>
-                             <td>Rp.90.000</td>
-                         </tr>
-                     </tbody>
-                     <tbody>
-                         <tr>
-                             <td>2</td>
-                             <td>11 April 2021</td>
-                             <td>samping ITB</td>
-                             <td>Rp.40.000</td>
-                         </tr>
-                     </tbody>
-                 </StyledTable>
+let idCustomer = 1;
+
+export const Pembayaran = () =>{
+    const [state, setState] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(`http://139.59.235.181:8800/payment`,);
+
+            setState(result.data);
+            console.log(result.data);
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div class="parent-container d-flex">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                            <Styledjudul>
+                                Riwayat Pembayaran Berhasil
+                            </Styledjudul>
+                            <StyledTable striped bordered hover>
+                            <thead>
+                                <tr>        
+                                    <th >Order id</th>
+                                    <th >amount</th>
+                                    <th >date</th>
+                                </tr>
+                            </thead >
+                            <tbody >
+                            {state.data?.map(item => (
+                                <tr>
+                                    <td>{item.order_id}</td>
+                                    <td>{item.amount}</td>
+                                    <td>{item.payment_date.substring(0,10)}</td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </StyledTable>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9">
+                    <StyledModal >
+                        <Modal.Header style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                }}>
+                            <Modal.Title>Nominal tagihan</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                }}>
+                            <p>Rp.50.000</p>
+                        </Modal.Body>
+                    </StyledModal>
+                    <Styledjudul2>
+                        Pilih metode pembayaran
+                    </Styledjudul2>
+                    <Styledgroupcard>
+                        <Card>
+                            <Card.Img variant="bottom" src={ovoImage} />
+                            <Card.Body>
+                                <Styledpembayaran1>Ovo</Styledpembayaran1>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="bottom" src={gopayImage} />
+                            <Card.Body>
+                                <Styledpembayaran2>Gopay</Styledpembayaran2>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="bottom" src={linkajaImage} />
+                            <Card.Body>
+                                <Styledpembayaran3>LinkAja</Styledpembayaran3>
+                            </Card.Body>
+                        </Card>
+                    </Styledgroupcard>
+                    <Petunjuk>
+                        Petunjuk Pembayaran:
+                    </Petunjuk>
+                    <Isi>
+                        1. saya saya
+                    </Isi>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-9">
-                <StyledModal >
-                    <Modal.Header style={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            }}>
-                        <Modal.Title>Nominal tagihan</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body style={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            }}>
-                        <p>Rp.50.000</p>
-                    </Modal.Body>
-                </StyledModal>
-                <Styledjudul2>
-                    Pilih metode pembayaran
-                </Styledjudul2>
-                <Styledgroupcard>
-                    <Card>
-                        <Card.Img variant="bottom" src={ovoImage} />
-                        <Card.Body>
-                            <Styledpembayaran1>Ovo</Styledpembayaran1>
-                        </Card.Body>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="bottom" src={gopayImage} />
-                        <Card.Body>
-                            <Styledpembayaran2>Gopay</Styledpembayaran2>
-                        </Card.Body>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="bottom" src={linkajaImage} />
-                        <Card.Body>
-                            <Styledpembayaran3>LinkAja</Styledpembayaran3>
-                        </Card.Body>
-                    </Card>
-                </Styledgroupcard>
-                <Petunjuk>
-                    Petunjuk Pembayaran:
-                </Petunjuk>
-                <Isi>
-                    1. saya saya
-                </Isi>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                </div>
-            </div>
-        </div>
-    </div>
-)
+    );
+}
